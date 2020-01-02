d70e8ba1fac28d02920bd7eb63eaf0ee0afe5923
Lê Thị Trang <57514504+trang-trang@users.noreply.github.com>
2020-01-02 07:41:28 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
-#include <stdio.h>// in ra giai thừa của một số nguyên
+#include<stdio.h>
 int main()
 {
-    int n;
-    printf("Enter value of n: ");
-    scanf("%d", &n);
-    int giaithua(int n);
-    printf("the factorial value is: %d \n", giaithua(n));
-}
-int giaithua(int n)
-{
-    int gt = 1;
-    int i;
-    for(i = n; i > 0;i--)
-        gt = gt *i;
-    return gt;
+    int R, s, c;
+    float pi = 3.14;
+    printf("\n Enter radius of circle: ");
+    scanf("%d",&R);
+    s = pi * R * R;
+    c = (pi*R)*1/2;
+    printf("\n Enter the perimeter and the area : %d %d",s, c);
+    scanf("%d", &R);
+    return 0;
 }
